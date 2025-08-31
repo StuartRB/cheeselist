2dced666469bf652ab0b46ef4ac82ba893cf9a60
Stuart Burnside <stuartburnside@MacBookAir.broadband>
2025-08-31 11:45:13 +0100

    try api call

--- src/CheeseList.js
+++ src/CheeseList.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 const CheeseList = () => {
+
   const cheeses = ["cheddar", "brie", "manchego", "comte", "stilton"]
 
-  return (
-      <ul>
-        {cheeses.map((cheese, index) => (
-          <li key={index}>{cheese}</li>
-        ))}
-      </ul>
+    const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/get")   // replace with your API endpoint
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((err) => console.error("Error fetching:", err));
+  }, []); // empty array = run once when component mounts
+
+  return (<>
+          <ul>
+            {cheeses.map((cheese, index) => (
+              <li key={index}>{cheese}</li>
+            ))}
+          </ul>
+
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+      </>
     );
 };
 
